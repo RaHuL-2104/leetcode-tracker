6bfc872285a1d85e24ad9509133008db43aa44b1
Rahul Mishra <iamrahulm650@gmail.com>
2024-11-29 17:15:31 +0000

    Code challenge submission for question: 653 Two Sum IV - Input is a BST

---
+++ 653 Two Sum IV - Input is a BST/653two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,59 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+ class BSTIterator {
+    stack<TreeNode*> st;
+    bool reverse = true;
+public:
+    BSTIterator(TreeNode* root, bool isReverse) {
+        reverse = isReverse;
+        pushAll(root);
+    }
+    
+    int next() {
+        TreeNode* tmpNode = st.top();
+        st.pop();
+        if(!reverse) pushAll(tmpNode->right);
+        else pushAll(tmpNode->left);
+        return tmpNode->val;
+    }
+    
+    bool hasNext() {
+        return !st.empty();
+    }
+private:
+    void pushAll(TreeNode* node){
+        for(;node != NULL;){
+            st.push(node);
+            if(reverse){
+                node = node->right;
+           } else {
+                node = node->left;
+           }
+        }
+    }
+};
+class Solution {
+public:
+    bool findTarget(TreeNode* root, int k) {
+        if(!root) return false;
+         BSTIterator l(root,false);
+         BSTIterator r(root,true);
+         int i = l.next();
+         int j = r.next();
+         while(i<j){
+            if(i+j == k) return true;
+            else if(i + j < k) i = l.next();
+            else j = r.next();
+         }
+         return false;
+    }
+};
