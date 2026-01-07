39d8761f3fc84edd198121fc057f656b141118dc
Rahul Mishra <iamrahulm650@gmail.com>
2026-01-07 21:44:40 +0000

    Code challenge submission for question: 88 Merge Sorted Array

---
+++ 88 Merge Sorted Array/88merge-sorted-array.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // for(int j = 0, i = m; j < n; j++){
+        //     nums1[i] = nums2[j];
+        //     i++;
+        // }
+        // Arrays.sort(nums1);
+        int last = m + n -1;
+        while(m > 0 && n > 0){
+            if(nums1[m-1] > nums2[n-1]){
+                nums1[last] = nums1[m-1];
+                m--;
+            } else{
+                nums1[last] = nums2[n-1];
+                n--;
+            }
+            last--;
+        }
+        while(n > 0){
+            nums1[last] = nums2[n-1];
+            n--;
+            last--;
+        }
+
+        
+    }
+}
