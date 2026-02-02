c4febcdf529448325544eb3fe2b85f9fd95e5cdb
Rahul Mishra <iamrahulm650@gmail.com>
2026-02-02 03:40:34 +0000

    Code challenge submission for question: 141 Linked List Cycle

---
+++ 141 Linked List Cycle/141linked-list-cycle.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // concept of fast and slow pointers 
+        // if there is a loop there is going to be a situation where both the pointers collide
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow == fast) return true;
+        }
+        return false;
+
+    }
+}
