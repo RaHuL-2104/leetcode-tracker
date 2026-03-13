c471975dcf862a06927be7a868966944351b4775
Rahul Mishra <iamrahulm650@gmail.com>
2026-03-13 20:06:40 +0000

    Code challenge submission for question: 345 Reverse Vowels of a String

---
+++ 345 Reverse Vowels of a String/345reverse-vowels-of-a-string.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean isVowel(char c){
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+    public String reverseVowels(String s) {
+        char[] ch = s.toCharArray();
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right){
+            while(left < right && !isVowel(ch[left])){
+                left++;
+            }
+            while(left < right && !isVowel(ch[right])){
+                right--;
+            }
+            char temp = ch[left];
+            ch[left] = ch[right];
+            ch[right] = temp;
+
+            left++;
+            right--;
+        }
+        return new String(ch);
+    }
+}
