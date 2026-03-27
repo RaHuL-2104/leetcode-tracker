8da916cd68980d5d9fefb237161868c57d72494c
Rahul Mishra <121811305+RaHuL-2104@users.noreply.github.com>
2026-03-27 17:28:46 +0530

    Time: 1 ms (78.05%) | Memory: 42.3 MB (74.91%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,21 +1,37 @@
 class Solution {
+    private int getNext(int n){
+        int sum = 0;
+        while(n > 0){
+            sum += Math.pow(n%10, 2.0);
+            n /= 10;
+        }
+        return sum;
+    }
     public boolean isHappy(int n) {
         // Using hashset to keep a track of the numbers
         // If number isn't happy, there are chances of the number being repeated again.
-        HashSet<Integer> hs = new HashSet<>();
-        while(true){
-            int sum = 0;
-            while(n != 0){
-                int d = n%10;
-                sum += Math.pow(d, 2.0);
-                n = n/10;
-            }
-            if(sum == 1) return true;
-            n = sum;
-            if(hs.contains(n)){
-                return false;
-            }
-            hs.add(n);
+        // HashSet<Integer> hs = new HashSet<>();
+        // while(true){
+        //     int sum = 0;
+        //     while(n != 0){
+        //         int d = n%10;
+        //         sum += Math.pow(d, 2.0);
+        //         n = n/10;
+        //     }
+        //     if(sum == 1) return true;
+        //     n = sum;
+        //     if(hs.contains(n)){
+        //         return false;
+        //     }
+        //     hs.add(n);
+        // }
+        // Similar concept with the help of two pointers
+        int slow = n;
+        int fast = getNext(n);
+        while(fast != 1 && slow != fast){
+            slow = getNext(slow);
+            fast = getNext(getNext(fast));
         }
+        return fast == 1;
     }
 }
