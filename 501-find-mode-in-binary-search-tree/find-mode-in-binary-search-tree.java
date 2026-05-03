8ff2cf81dea71893a4381e3b314a907a03eb97b4
Rahul Mishra <121811305+RaHuL-2104@users.noreply.github.com>
2026-05-03 10:45:29 +0530

    Time: 0 ms (100.00%) | Memory: 47.1 MB (36.78%) - LeetSync

--- 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -14,35 +14,74 @@
  * }
  */
 class Solution {
-    private Map<Integer, Integer> mp = new HashMap<>();
+    // private Map<Integer, Integer> mp = new HashMap<>();
 
-    public void inorder(TreeNode node){
+    // public void inorder(TreeNode node){
+    //     if(node == null) return;
+    //     inorder(node.left);
+    //     mp.put(node.val, mp.getOrDefault(node.val, 0) + 1);
+    //     inorder(node.right);
+    // }
+    // public int[] findMode(TreeNode root) {
+    //     List<Integer> res = new ArrayList<>();
+    //     inorder(root);
+    //     int maxFreq = 0;
+
+    //     for(Map.Entry<Integer, Integer> e : mp.entrySet()){
+    //         int key = e.getKey();
+    //         int val = e.getValue();
+
+    //         if(val > maxFreq){
+    //             maxFreq = val;
+    //             res.clear();
+    //             res.add(key);
+    //         } else if(val == maxFreq){
+    //             res.add(key);
+    //         }
+    //     }
+
+
+    //     int[] result = new int[res.size()];
+    //     for(int i = 0; i < result.length; i++){
+    //         result[i] = res.get(i);
+    //     }
+    //     return result;
+    // }
+
+    /** WITHOUT EXTRA SPACE */
+    private int currNum = 0;
+    private int currStreak = 0;
+    private int maxStreak = 0;
+    private List<Integer> res = new ArrayList<>();
+
+    private void dfs(TreeNode node){
         if(node == null) return;
-        inorder(node.left);
-        mp.put(node.val, mp.getOrDefault(node.val, 0) + 1);
-        inorder(node.right);
+        dfs(node.left);
+        if(node.val == currNum){
+            currStreak++;
+        } else{
+            currNum = node.val;
+            currStreak = 1;
+        }
+
+        if(currStreak > maxStreak){
+            res.clear();
+            maxStreak = currStreak;
+        }
+
+        if(currStreak == maxStreak){
+            res.add(node.val);
+        }
+        dfs(node.right);
     }
-    public int[] findMode(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        inorder(root);
-        int maxFreq = 0;
 
-        for(Map.Entry<Integer, Integer> e : mp.entrySet()){
-            int key = e.getKey();
-            int val = e.getValue();
+    public int[] findMode(TreeNode root){
+        dfs(root);
 
-            if(val > maxFreq){
-                maxFreq = val;
-                res.clear();
-                res.add(key);
-            } else if(val == maxFreq){
-                res.add(key);
-            }
+        int[] resultArray = new int[res.size()];
+        for(int i = 0; i < res.size(); i++){
+            resultArray[i] = res.get(i);
         }
-        int[] result = new int[res.size()];
-        for(int i = 0; i < result.length; i++){
-            result[i] = res.get(i);
-        }
-        return result;
+        return resultArray;
     }
 }
