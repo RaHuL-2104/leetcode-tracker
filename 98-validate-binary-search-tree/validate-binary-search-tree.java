4f0fe56a9c302eeb827a61e494207c5adcab4639
Rahul Mishra <121811305+RaHuL-2104@users.noreply.github.com>
2026-05-08 01:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (64.14%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -14,26 +14,14 @@
  * }
  */
 
- // inorder traversal of this tree and check whether the tree is sorted or not
+ // Maintaining and updating a range as we traverse through the tree
 class Solution {
-    public ArrayList<Integer> inorder(TreeNode root, ArrayList<Integer> res){
-        if(root == null) return res;
-        inorder(root.left, res);
-        res.add(root.val);
-        inorder(root.right, res);
-        return res;
-    }
     public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal){
         if(root == null) return true;
-        ArrayList<Integer> res = new ArrayList<>();
-        inorder(root, res);
-        int f = 0;
-        for(int i = 1; i < res.size(); i++){
-            if(res.get(i) <= res.get(i-1)){
-                return false;
-            }
-        }
-        return true;
+        if(root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
-// 1 5 3 4 6
